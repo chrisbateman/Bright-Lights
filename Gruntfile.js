7b0aab277417997e99696cfc52d1309c2c410c49
Chris Bateman <chrisb808@gmail.com>
2014-04-09 02:00:05 -0400

    Grunt! Closes #5

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        meta: {
+            banner: '/**\n' +
+                    //' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                    ' * <%= pkg.name %> - v<%= pkg.version %>\n' +
+                    ' * <%= pkg.homepage %>\n' +
+                    ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                    ' * Licensed <%= pkg.licenses.type %> <<%= pkg.licenses.url %>>\n' +
+                    ' */\n'
+        },
+        concat: {
+            js: {
+                src: ['src/hand.js', 'src/external.js', 'src/main.js'],
+                dest: 'build/app.js',
+            },
+        },
+        uglify: {
+            build: {
+                src: 'build/app.js',
+                dest: 'build/app.min.js'
+            },
+            options: {
+                banner: '<%= meta.banner %>'
+            }
+        },
+        cssmin: {
+            add_banner: {
+                options: {
+                    banner: '<%= meta.banner %>'
+                },
+                files: {
+                    'build/styles.min.css': ['src/*.css']
+                }
+            }
+        },
+        copy: {
+            folders: {
+                cwd: 'src',
+                src: ['fonts/**', 'img/**'],
+                dest: 'build',
+                expand: true
+            },
+        },
+        clean: {
+            js: {
+                src: ['build/app.js']
+            }
+        },
+        processhtml: {
+            build: {
+                files: {
+                    'build/index.htm': ['src/index.htm']
+                }
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-processhtml');
+    
+    
+    grunt.registerTask('default', ['copy', 'concat', 'uglify', 'cssmin', 'clean', 'processhtml']);
+    
+};
